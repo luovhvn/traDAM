9f1ed3c7679e9482395814267949ae3c605a2acc
Mañana <Maana@192.168.13.23>
2024-10-02 12:45:15 +0200

    AreaRectangulo

---
+++ java/src/java/variables/AreaRec.java
@@ -0,0 +1,13 @@
+package java.variables;
+
+public class AreaRec {
+	public static void main (String[] ags) {
+		
+		int alto = 14;
+		int ancho = 23;
+		
+		int area = alto * ancho;
+		
+		System.out.println("El area del rectángulo es " + area);
+	}
+}
