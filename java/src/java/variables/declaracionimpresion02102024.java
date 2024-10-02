4f7d3749e382eeab081fc963450eeca87a545b26
Mañana <Maana@192.168.13.23>
2024-10-02 12:26:40 +0200

    DeclaracionVariables

---
+++ java/src/java/variables/declaracionimpresion02102024.java
@@ -0,0 +1,19 @@
+package java.variables;
+
+public class declaracionimpresion02102024 {
+	public static void main (String[] ags) {
+	
+	int numentero=5;
+	double numdecimal=6.3;
+	String cadena="Hala Madrid";
+	
+	System.out.println("El número entero es: " + numentero);
+	System.out.println("El número decimal es: " + numdecimal);
+	System.out.println("La cadena es: " + cadena);
+	
+	}
+	
+}
+
+				
+ 
