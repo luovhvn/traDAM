417a6f8aeb7cf39ba47f05e271795dd90b01611d
Mañana <Maana@192.168.13.23>
2024-10-02 12:52:47 +0200

    CelsiusaFahrenheit

---
+++ java/src/java/variables/CelciusFaren.java
@@ -0,0 +1,13 @@
+package java.variables;
+
+public class CelciusFaren {
+	public static void main (String[] ags) {
+		
+		int celsius = 80;
+		
+		int fahren = (celsius * 9/5) + 32;
+		
+		System.out.println(celsius + " Cº son " + fahren + " Fº");
+			
+	}
+}
