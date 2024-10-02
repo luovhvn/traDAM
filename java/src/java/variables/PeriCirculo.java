f53575dfc285b0e9f5a4fad1c7c09b148a10defd
Mañana <Maana@192.168.13.23>
2024-10-02 12:58:09 +0200

    PerimetroCirculo

---
+++ java/src/java/variables/PeriCirculo.java
@@ -0,0 +1,13 @@
+package java.variables;
+
+public class PeriCirculo {
+	public static void main (String[] ags) {
+	
+		double radio = 7.4;
+		
+		double perimetro = 2 * 3.14 * radio;
+		
+		System.out.println("El perímetro del círculo es " + perimetro);
+				
+	}
+}
