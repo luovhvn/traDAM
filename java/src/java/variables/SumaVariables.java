797c175adea783f5b4484aad35469e87882be888
Mañana <Maana@192.168.13.23>
2024-10-02 12:38:39 +0200

    SumaVariables

---
+++ java/src/java/variables/SumaVariables.java
@@ -0,0 +1,13 @@
+package java.variables;
+
+public class SumaVariables {
+	public static void main (String[] ags) {
+		
+		int num1 = 4;
+		int num2 = 3;
+		
+		int suma = num1 + num2;
+		
+		System.out.println("El resultado de la suma es ", suma);
+	}
+}
