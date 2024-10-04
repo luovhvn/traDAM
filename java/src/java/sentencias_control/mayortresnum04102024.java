00db1bd9e206da2110d759dadd724122595a39d5
Mañana <Maana@192.168.13.23>
2024-10-04 12:38:05 +0200

    MayorTresNum

---
+++ java/src/java/sentencias_control/mayortresnum04102024.java
@@ -0,0 +1,28 @@
+package java.sentencias_control;
+
+import java.util.Scanner;
+
+public class mayortresnum04102024 {
+		public static void main (String[] args) {
+			
+			Scanner scan = new Scanner(System.in);
+	
+
+			System.out.println("Introduce el primer número");
+			int num1 = scan.nextInt();
+			System.out.println("Introduce el segundo número");
+			int num2 = scan.nextInt();
+			System.out.println("Introduce el tercer número");
+			int num3 = scan.nextInt();
+			
+			int mayor = num1;
+			
+			if(num2 > mayor) {
+				mayor = num2;
+			}
+			if(num3 > mayor) {
+				mayor = num3;
+			}
+			System.out.println("El número mayor es " + mayor);
+		}
+}
