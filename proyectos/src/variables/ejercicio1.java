4b3429e07ec9e673dc67da3c28cc7c0697262fa1
luisr <luisr@192.168.13.98>
2024-10-16 11:59:46 +0200

    DeclaracionImpresionVariables

--- proyectos/src/variables/ejercicio1.java
+++ proyectos/src/variables/ejercicio1.java
@@ -3,9 +3,13 @@ package variables;
 public class ejercicio1 {
 		public static void main (String[] args) {
 			
-			int[] numeros = new int[5];
+			int num = 7;
+			String cadena = "HalaMadrid";
+			double deci = 4.9;
 			
-
+			System.out.println("El número es " + num);
+			System.out.println("La cadena es " + cadena);
+			System.out.println("El decimal es " + deci);
 			
 		}
 }
