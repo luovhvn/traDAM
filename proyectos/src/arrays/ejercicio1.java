761ecf8baefbd05daf0a734f8671152f63663858
luisr <luisr@192.168.13.98>
2024-10-16 11:49:36 +0200

    NumParImpar

--- proyectos/src/arrays/ejercicio1.java
+++ proyectos/src/arrays/ejercicio1.java
@@ -1,5 +1,20 @@
 package arrays;
 
-public class ejercicio1 {
+import java.util.Scanner;
 
+public class ejercicio1 {
+		public static void main (String [] args) {
+			
+			Scanner scan = new Scanner(System.in);
+			
+			System.out.println("Escribe un número");
+			int num = scan.nextInt();
+			
+			if (num % 2 == 0) {
+				System.out.println("El número es par");
+			} else {
+				System.out.println("El número es impar");
+			}
+			
+		}
 }
