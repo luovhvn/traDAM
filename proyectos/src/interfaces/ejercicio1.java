c0ebd69ca7b42987553524d47e91f7334fab1caa
luisr <luisr@192.168.13.98>
2024-10-17 11:41:09 +0200

    InterfazUsuario

--- proyectos/src/interfaces/ejercicio1.java
+++ proyectos/src/interfaces/ejercicio1.java
@@ -1,5 +1,70 @@
 package interfaces;
 
-public class ejercicio1 {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
+public class ejercicio1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ejercicio1 frame = new ejercicio1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ejercicio1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Contraseña");
+		lblNewLabel_1.setBounds(56, 97, 52, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(56, 121, 96, 19);
+		contentPane.add(passwordField);
+		
+		textField = new JTextField();
+		textField.setBounds(56, 64, 96, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Usuario");
+		lblNewLabel.setBounds(56, 41, 34, 13);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setBounds(172, 176, 85, 21);
+		contentPane.add(btnNewButton);
+	}
 }
--- proyectos/src/module-info.java
+++ proyectos/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module proyectos {
+	requires java.desktop;
 }
