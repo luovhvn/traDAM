cbca868548690f737cc17825f1ab59aebf109862
luisr <luisr@10.0.0.32>
2024-10-25 11:29:15 +0200

    ContarVocales

---
+++ proyectos/src/arrays/Contar_Vocales_en_un_Array_de_Caracteres.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class Contar_Vocales_en_un_Array_de_Caracteres {
+
+	public static void main(String[] args) {
+		String cadena = "dinosaurio";
+
+		char[] arrayCaracteres = new char[cadena.length()];
+		arrayCaracteres = cadena.toCharArray();
+		
+		int vocales = 0;
+		
+		for (int i = 0; i <= cadena.length() - 1; i++) {
+			if (arrayCaracteres[i] == 'a' || arrayCaracteres[i] == 'A'|| arrayCaracteres[i] == 'e' || arrayCaracteres[i] == 'E'
+					|| arrayCaracteres[i] == 'i' || arrayCaracteres[i] == 'I'|| arrayCaracteres[i] == 'o'||arrayCaracteres[i] == 'O'||
+					arrayCaracteres[i] == 'u'|| arrayCaracteres[i] == 'U') {
+				vocales++;
+			}
+		}
+		System.out.println("Tiene " + vocales + " vocales");
+	}
+
+}
+
+
