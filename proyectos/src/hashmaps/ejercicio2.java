6470b0f7a22b9809aac00c855c0d4a7b7f33c867
luisr <luisr@192.168.13.98>
2024-11-04 13:29:12 +0100

    NotasAlumnos

---
+++ proyectos/src/hashmaps/ejercicio2.java
@@ -0,0 +1,18 @@
+package hashmaps;
+
+import java.util.HashMap;
+
+public class ejercicio2 {
+		public static void main (String[] args) {
+			
+			HashMap<String, Integer> notas = new HashMap<>();
+			notas.put("Noriel", 5);
+			notas.put("Anuel", 3);
+			notas.put("Arcangel", 7);
+			notas.put("Benito", 5);
+			notas.put("Neutro", 1);
+			notas.put("Ryan", 9);
+			
+			
+		}
+}
