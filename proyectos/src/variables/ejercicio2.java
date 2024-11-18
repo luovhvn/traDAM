49a5ef3f60352c4b64139a88d8465be5767e31af
luisr <luisr@192.168.1.25>
2024-11-18 17:11:27 +0100

    SumaVariables

---
+++ proyectos/src/variables/ejercicio2.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class ejercicio2 {
+		public static void main (String[] args) {
+			
+			int num1 = 5, num2 = 3;
+			int suma = num1 + num2;
+			
+			System.out.println("La seuma de los números es " + suma);
+			
+		}
+}
