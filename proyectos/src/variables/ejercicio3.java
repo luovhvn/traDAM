17447b65310e4543815709ba6e730a3fcd50d4c7
luisr <luisr@192.168.1.25>
2024-11-18 17:17:42 +0100

    AreaRectangulo

---
+++ proyectos/src/variables/ejercicio3.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class ejercicio3 {
+		public static void main (String[] args) {
+			
+			int ancho = 12, alto = 3;
+			
+			int area = ancho * alto;
+			
+			System.out.println("El perímetro del rectángulo es " + ancho);
+			
+		}
+}
