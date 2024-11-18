41423a509c73b9920ffd50d0a86e9f10fecca171
luisr <luisr@192.168.1.25>
2024-11-18 17:48:11 +0100

    PromedioTresNum

---
+++ proyectos/src/variables/ejercicio7.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class ejercicio7 {
+		public static void main (String[] args) {
+			
+			int num1 = 7, num2 = 3, num3 = 16;
+			
+			double promedio = num1 + num2 + num3 / 3;
+			
+			System.out.println("El promedio de los números es " + promedio);
+			
+		}
+}
