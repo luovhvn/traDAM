2e3f90a4d571eb1ac69906f0631c2c2a3d396fe2
luisr <luisr@192.168.1.25>
2024-11-18 18:57:31 +0100

    SumaHasta100

---
+++ proyectos/src/sentencias_control/ejercicio5.java
@@ -0,0 +1,13 @@
+package sentencias_control;
+
+public class ejercicio5 {
+		public static void main (String[] args) {
+			
+			int suma = 0;
+			
+			for (int i = 1; i <= 100; i++) {
+				suma += i;
+			}
+			System.out.println("La suma es: " + suma);
+		}
+}
