65383843690692a15604119a443a21762ba5a27f
luisr <luisr@192.168.1.25>
2024-11-18 17:34:48 +0100

    PerimetroCirculo

---
+++ proyectos/src/variables/ejercicio5.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class ejercicio5 {
+		public static void main (String[] args) {
+			
+			int radio = 4;
+			
+			double perimetro = 2 * Math.PI * radio;
+			
+			System.out.println("El perímetro del círculo es " + perimetro);
+			
+		}
+}
