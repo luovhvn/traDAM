3bac6cee57a3345220f4696407b01f55ff45d4db
luisr <luisr@192.168.1.25>
2024-11-18 17:28:50 +0100

    CelsiusAFahren

---
+++ proyectos/src/variables/ejercicio4.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class ejercicio4 {
+		public static void main (String[] args) {
+		
+			int celsius = 80;
+			
+			int calcu = (celsius * 9/5) + 32;
+			
+			System.out.println(celsius + "º son " + calcu + " Fahrenheit");
+			
+		}
+}
