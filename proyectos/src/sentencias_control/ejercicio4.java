3c9b996eb63efa1b6e87e08e73d866e0cfbb1a86
luisr <luisr@192.168.1.25>
2024-11-18 18:53:36 +0100

    Print1al10

---
+++ proyectos/src/sentencias_control/ejercicio4.java
@@ -0,0 +1,11 @@
+package sentencias_control;
+
+public class ejercicio4 {
+		public static void main (String[] args) {
+			
+			for (int i = 1; i <= 10; i++) {
+				System.out.println(i);
+			}
+			
+		}
+}
