9e0c90e2ef2e4d7a6bfa7b5ac4b46ca24ec86fc6
luisr <luisr@192.168.1.25>
2024-11-18 18:01:40 +0100

    ConcatenarCadenas

---
+++ proyectos/src/variables/ejercicio9.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class ejercicio9 {
+		public static void main (String[] args) {
+			
+			String nombre = "Luis", apellido = "Rodríguez";
+			int edad = 18;
+			
+			System.out.println("Hola, mi nombre es " + nombre + " " + apellido + " y tengo " + edad + " años");
+		}
+}
